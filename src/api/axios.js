619de41c9fe96a45312b539fafa4eda04d1c1c60
ZeoXer <andrew17413@gmail.com>
2023-09-22 15:58:39 +0800

    add login and setToken api functions

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios"
 
 const service = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        common: {
+            "Authorization": ""
+        }
+    }
 })
 
 export default service
--- src/api/functions.js
+++ src/api/functions.js
@@ -2,6 +2,18 @@ import service from "./axios";
 
 const API_PATH = import.meta.env.VITE_API_PATH;
 
+export const signIn = (data) => {
+  return service({
+    url: "/v2/admin/signin",
+    method: "post",
+    data,
+  });
+};
+
+export const setAuthToken = (token) => {
+  service.defaults.headers.common["Authorization"] = token;
+};
+
 export const getAllProducts = () => {
   return service({
     url: `/v2/api/${API_PATH}/products/all`,
