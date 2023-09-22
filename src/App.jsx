a39f92081ebac8e72cd93953c6a3c6f57ef897b6
ZeoXer <andrew17413@gmail.com>
2023-09-22 15:57:18 +0800

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+
 const App = () => {
   return (
-    <div className="App">
-      <button type="button" className="btn btn-primary">
-        Primary
-      </button>
-      <button type="button" className="btn btn-secondary">
-        Secondary
-      </button>
-      <button type="button" className="btn btn-success">
-        Success
-      </button>
-      <button type="button" className="btn btn-danger">
-        Danger
-      </button>
-      <button type="button" className="btn btn-warning">
-        Warning
-      </button>
-      <button type="button" className="btn btn-info">
-        Info
-      </button>
-      <button type="button" className="btn btn-light">
-        Light
-      </button>
-      <button type="button" className="btn btn-dark">
-        Dark
-      </button>
-
-      <button type="button" className="btn btn-link">
-        Link
-      </button>
+    <div className="app">
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
 };
