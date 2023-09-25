1b2e5fe5765ccd298fcae6ce4604e8f017c052b9
ZeoXer <andrew17413@gmail.com>
2023-09-25 13:48:09 +0800

    add new route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/admin/Dashboard";
+import AdminProducts from "./pages/admin/AdminProducts";
 
 const App = () => {
   return (
     <div className="app">
       <Routes>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/admin" element={<Dashboard />}>
+          <Route path="products" element={<AdminProducts />}></Route>
+        </Route>
       </Routes>
     </div>
   );
