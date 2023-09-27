90c7f4ff039aa6e2f1fc6e5c22f20f31aa910eb6
ZeoXer <andrew17413@gmail.com>
2023-09-27 23:51:28 +0800

    add coupons route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/admin/Dashboard";
 import AdminProducts from "./pages/admin/AdminProducts";
+import AdminCoupons from "./pages/admin/AdminCoupons";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/admin" element={<Dashboard />}>
           <Route path="products" element={<AdminProducts />}></Route>
+          <Route path="coupons" element={<AdminCoupons />}></Route>
         </Route>
       </Routes>
     </div>
