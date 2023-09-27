a233b99890bff0d332d691179e1f4abdbb080f24
ZeoXer <andrew17413@gmail.com>
2023-09-27 23:52:03 +0800

    add coupons pages

---
+++ src/components/CouponModal.jsx
@@ -0,0 +1,168 @@
+import { useState, useEffect } from "react";
+import { addNewCoupon, editCouponById } from "../api/functions";
+
+const CREATE_COUPON = "create-coupon";
+const EDIT_COUPON = "edit-coupon";
+
+const CouponModal = ({ closeCouponModal, fetchCoupons, tempCoupon, type }) => {
+  const [tempData, setTempData] = useState({
+    title: "",
+    is_enabled: 0,
+    percent: 0,
+    due_date: 1555459200,
+    code: "",
+  });
+
+  useEffect(() => {
+    if (type === CREATE_COUPON) {
+      setTempData({
+        title: "",
+        is_enabled: 0,
+        percent: 0,
+        due_date: 1555459200,
+        code: "",
+      });
+    } else if (type === EDIT_COUPON) {
+      setTempData(tempCoupon);
+    }
+  }, [type, tempCoupon]);
+
+  const submit = async () => {
+    try {
+      let res;
+      if (type === CREATE_COUPON) {
+        res = await addNewCoupon(tempData);
+      } else if (type === EDIT_COUPON) {
+        res = await editCouponById(tempCoupon.id, tempData);
+      }
+      if (res.data.success) {
+        closeCouponModal();
+        fetchCoupons();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (["percent"].includes(name)) {
+      setTempData({ ...tempData, [name]: +value });
+    } else if (name === "is_enabled") {
+      setTempData({ ...tempData, [name]: +e.target.checked });
+    } else {
+      setTempData({ ...tempData, [name]: value });
+    }
+  };
+
+  return (
+    <div
+      className="modal fade"
+      id="couponModal"
+      tabIndex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-lg">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="exampleModalLabel">
+              {type === CREATE_COUPON && "建立新優惠券"}
+              {type === EDIT_COUPON && `編輯 ${tempData.title}`}
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              aria-label="Close"
+              onClick={closeCouponModal}
+            />
+          </div>
+          <div className="modal-body">
+            <div className="mb-2">
+              <label className="w-100" htmlFor="title">
+                標題
+                <input
+                  type="text"
+                  id="title"
+                  placeholder="請輸入標題"
+                  name="title"
+                  className="form-control mt-1"
+                  value={tempData.title}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+            <div className="row">
+              <div className="col-md-6 mb-2">
+                <label className="w-100" htmlFor="percent">
+                  折扣（%）
+                  <input
+                    type="text"
+                    name="percent"
+                    id="percent"
+                    placeholder="請輸入折扣（%）"
+                    className="form-control mt-1"
+                    value={tempData.percent}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+              <div className="col-md-6 mb-2">
+                <label className="w-100" htmlFor="due_date">
+                  到期日
+                  <input
+                    type="date"
+                    id="due_date"
+                    name="due_date"
+                    placeholder="請輸入到期日"
+                    className="form-control mt-1"
+                  />
+                </label>
+              </div>
+              <div className="col-md-6 mb-2">
+                <label className="w-100" htmlFor="code">
+                  優惠碼
+                  <input
+                    type="text"
+                    id="code"
+                    name="code"
+                    placeholder="請輸入優惠碼"
+                    className="form-control mt-1"
+                    value={tempData.code}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
+            </div>
+            <label className="form-check-label" htmlFor="is_enabled">
+              <input
+                className="form-check-input me-2"
+                type="checkbox"
+                id="is_enabled"
+                name="is_enabled"
+                checked={!!tempData.is_enabled}
+                onChange={handleChange}
+              />
+              是否啟用
+            </label>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={closeCouponModal}
+            >
+              關閉
+            </button>
+            <button type="button" className="btn btn-primary" onClick={submit}>
+              儲存
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CouponModal;
---
+++ src/pages/admin/AdminCoupons.jsx
@@ -0,0 +1,137 @@
+import { useEffect, useState, useRef } from "react";
+import { deleteCouponById, getAdminCoupons } from "../../api/functions";
+import { Modal } from "bootstrap";
+import DeleteModal from "../../components/DeleteModal";
+import Pagination from "../../components/Pagination";
+import CouponModal from "../../components/CouponModal";
+
+const CREATE_COUPON = "create-coupon";
+const EDIT_COUPON = "edit-coupon";
+
+const AdminCoupons = () => {
+  const [coupons, setCoupons] = useState([]);
+  const [pagination, setPagination] = useState({});
+  const [type, setType] = useState(CREATE_COUPON);
+  const [tempCoupon, setTempCoupon] = useState({});
+
+  const couponModal = useRef();
+  const deleteModal = useRef();
+
+  useEffect(() => {
+    couponModal.current = new Modal("#couponModal", {
+      backdrop: "static",
+    });
+
+    deleteModal.current = new Modal("#deleteModal", {
+      backdrop: "static",
+    });
+
+    fetchCoupons();
+  }, []);
+
+  const fetchCoupons = async (page) => {
+    const couponRes = await getAdminCoupons(page);
+    setCoupons(couponRes.data.coupons);
+    setPagination(couponRes.data.pagination);
+  };
+
+  const openCouponModal = (type, coupon) => {
+    setType(type);
+    setTempCoupon(coupon);
+    couponModal.current.show();
+  };
+
+  const closeCouponModal = () => {
+    couponModal.current.hide();
+  };
+
+  const openDeleteModal = (product) => {
+    setTempCoupon(product);
+    deleteModal.current.show();
+  };
+
+  const closeDeleteModal = () => {
+    deleteModal.current.hide();
+  };
+
+  const deleteCoupon = async (id) => {
+    try {
+      const { data } = await deleteCouponById(id);
+      if (data.success) {
+        closeDeleteModal();
+        fetchCoupons();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="p-3">
+      <CouponModal
+        closeCouponModal={closeCouponModal}
+        fetchCoupons={fetchCoupons}
+        tempProduct={tempCoupon}
+        type={type}
+      />
+      <DeleteModal
+        closeDeleteModal={closeDeleteModal}
+        text={tempCoupon.title}
+        deleteFunc={() => deleteCoupon(tempCoupon.id)}
+      />
+      <h3>優惠券列表</h3>
+      <hr />
+      <div className="text-end">
+        <button
+          type="button"
+          className="btn btn-primary btn-sm"
+          onClick={() => openCouponModal(CREATE_COUPON, {})}
+        >
+          建立新優惠券
+        </button>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">名稱</th>
+            <th scope="col">折扣</th>
+            <th scope="col">到期日</th>
+            <th scope="col">啟用狀態</th>
+            <th scope="col">編輯</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coupons.map((coupon) => {
+            return (
+              <tr key={coupon.id}>
+                <td>{coupon.title}</td>
+                <td>{coupon.percent}</td>
+                <td>{coupon.due_date}</td>
+                <td>{coupon.is_enabled ? "啟用" : "未啟用"}</td>
+                <td>
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-sm"
+                    onClick={() => openCouponModal(EDIT_COUPON, coupon)}
+                  >
+                    編輯
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-outline-danger btn-sm ms-2"
+                    onClick={() => openDeleteModal(coupon)}
+                  >
+                    刪除
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Pagination pagination={pagination} changePage={fetchCoupons} />
+    </div>
+  );
+};
+
+export default AdminCoupons;
