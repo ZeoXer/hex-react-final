e5121a3f9dc24584e4955f303ec909964dea1871
ZeoXer <andrew17413@gmail.com>
2023-10-10 00:04:27 +0800

    add checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Home from "./pages/front/Home";
 import Products from "./pages/front/Products";
 import ProductDetail from "./pages/front/ProductDetail";
 import Cart from "./pages/front/Cart";
+import Checkout from "./pages/front/Checkout";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="products" element={<Products />}></Route>
           <Route path="product/:id" element={<ProductDetail />}></Route>
           <Route path="cart" element={<Cart />}></Route>
+          <Route path="checkout" element={<Checkout />}></Route>
         </Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/admin" element={<Dashboard />}>
