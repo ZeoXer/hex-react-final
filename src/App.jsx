3d26b065adfd944a8d8d28598bc331a87f0255c4
ZeoXer <andrew17413@gmail.com>
2024-01-15 21:25:53 +0800

    modify router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/admin/Dashboard";
 import AdminProducts from "./pages/admin/AdminProducts";
@@ -14,29 +14,24 @@ import CheckoutSuccess from "./pages/front/CheckoutSuccess";
 
 const App = () => {
   return (
-    <BrowserRouter basename="/hex-react-final">
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<FrontLayout />}>
-            <Route path="" element={<Home />}></Route>
-            <Route path="products" element={<Products />}></Route>
-            <Route path="product/:id" element={<ProductDetail />}></Route>
-            <Route path="cart" element={<Cart />}></Route>
-            <Route path="checkout" element={<Checkout />}></Route>
-            <Route
-              path="success/:orderId"
-              element={<CheckoutSuccess />}
-            ></Route>
-          </Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/admin" element={<Dashboard />}>
-            <Route path="products" element={<AdminProducts />}></Route>
-            <Route path="coupons" element={<AdminCoupons />}></Route>
-            <Route path="orders" element={<AdminOrders />}></Route>
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<FrontLayout />}>
+          <Route path="" element={<Home />}></Route>
+          <Route path="products" element={<Products />}></Route>
+          <Route path="product/:id" element={<ProductDetail />}></Route>
+          <Route path="cart" element={<Cart />}></Route>
+          <Route path="checkout" element={<Checkout />}></Route>
+          <Route path="success/:orderId" element={<CheckoutSuccess />}></Route>
+        </Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/admin" element={<Dashboard />}>
+          <Route path="products" element={<AdminProducts />}></Route>
+          <Route path="coupons" element={<AdminCoupons />}></Route>
+          <Route path="orders" element={<AdminOrders />}></Route>
+        </Route>
+      </Routes>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/all.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
